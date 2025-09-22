35f0b17b18a9da7adeac6a4ff02e65d18b3aeef2
Mauli Radhika <mauliradhika05@gmail.com>
2025-09-23 00:33:30 +0530

    23-09-25

---
+++ globals.js
@@ -0,0 +1,15 @@
+//GLOBALS - NO WINDOW
+
+// __dirname -> path to current directory
+// __filename -> file name
+// require -> function to use modules (CommonJS)
+// module -> information about current module (file)
+// process -> information about env where the program is being executed
+
+console.log(__dirname) //C:\Users\KIIT0001\OneDrive\Desktop\Git\node-express-learning
+console.log(__filename)//C:\Users\KIIT0001\OneDrive\Desktop\Git\node-express-learning\1-introduction.js
+
+setInterval( () => {
+    console.log("Hello!!!")
+}, 1000) //keeps printing in interval of 1 second
+
